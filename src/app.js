0974f021657ec47187bc246a14b811b189c4d43e
AntonioMora <126864866+Antoniomorales17@users.noreply.github.com>
2023-05-13 11:00:49 +0000

    commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,10 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const card = {
+    hearth: [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"],
+    sword: [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"],
+    diamond: [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"],
+    trebol: [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"]
+  };
 };
